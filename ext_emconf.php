e22c51fa2a39f83d98ee938381c7c98d87f3a4a7
Michael Blunck <michidrau@gmail.com>
2023-02-22 11:41:45 +0100

    [TASK] Initial commit

---
+++ ext_emconf.php
@@ -0,0 +1,16 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'cozy_backend',
+    'description' => 'Accompanying extension for the TYPO3 housekeeping presentation',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '10.4.0-12.4.99',
+        ],
+    ],
+    'autoload' => [
+        'psr-4' => [
+            'Mblunck\\CozyBackend\\' => 'Classes/',
+        ],
+    ],
+];
