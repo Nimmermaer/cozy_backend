6fc3adff4abd41893307087185f15e56be5dbd9b
Michael B <michidrau@gmail.com>
2023-02-22 14:18:26 +0100

    [TASK] Add backend css for live and dev

--- ext_tables.php
+++ ext_tables.php
@@ -3,11 +3,16 @@ declare(strict_types=1);
 
 
 defined('TYPO3') || die();
-if(\TYPO3\CMS\Core\Core\Environment::getContext()->isDevelopment()) {
+if (\TYPO3\CMS\Core\Core\Environment::getContext()->isDevelopment()) {
     $GLOBALS['TBE_STYLES']['skins']['cozy_backend'] = [
-        'name' => 'cozy_backend',
         'stylesheetDirectories' => [
-            'css' => 'EXT:cozy_backend/Resources/Public/Backend/Css/'
+            'css' => 'EXT:cozy_backend/Resources/Public/Backend/Css/Dev/'
+        ]
+    ];
+} else {
+    $GLOBALS['TBE_STYLES']['skins']['cozy_backend'] = [
+        'stylesheetDirectories' => [
+            'css' => 'EXT:cozy_backend/Resources/Public/Backend/Css/Live/'
         ]
     ];
 }
