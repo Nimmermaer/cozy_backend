9445a37794dcbbb3530dbe74c631245733a1136b
Michael B <michidrau@gmail.com>
2023-02-22 22:18:35 +0100

    [TASK] Configure small plugin without extbase

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,8 +1,8 @@
 <?php
 
 declare(strict_types=1);
-use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 
+use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 defined('TYPO3') || die();
