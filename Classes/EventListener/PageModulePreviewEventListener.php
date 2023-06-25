f9f575b08c1323931b7919bb8483a51dcf5ceaaa
Michael B <michidrau@gmail.com>
2023-06-26 00:37:06 +0200

    [TASK] Moves eventlistener

--- Classes/EventListener/PageModulePreviewEventListener.php
+++ Classes/EventListener/PageModulePreviewEventListener.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Mblunck\CozyBackend\Provider;
+namespace Mblunck\CozyBackend\EventListener;
 
 use Mblunck\CozyBackend\Renderer\PagePropertiesRenderer;
 use TYPO3\CMS\Backend\Controller\Event\ModifyPageLayoutContentEvent;
@@ -12,7 +12,7 @@ use TYPO3\CMS\Backend\Controller\Event\ModifyPageLayoutContentEvent;
  *
  * @internal This is a specific listener implementation and is not considered part of the Public TYPO3 API.
  */
-final class PageModuleProvider
+final class PageModulePreviewEventListener
 {
     public function __construct(
         protected readonly PagePropertiesRenderer $pageRenderer
