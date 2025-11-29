da1d1e52898ab63020ea164d85c3b1c4c2713c6e
Michael Blunck <michael.blunck@webicorns.com>
2025-11-29 11:29:28 +0100

    [TASK] adds fluid components
    adds paths for components

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -6,6 +6,6 @@ use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 defined('TYPO3') || die();
 
-call_user_func(static function (): void {
-    ExtensionManagementUtility::addStaticFile('cozy_backend', 'Configuration/TypoScript', 'cozy_backend');
-});
+call_user_func(static function ($table, $extensionKey): void {
+    ExtensionManagementUtility::addStaticFile($extensionKey, 'Configuration/TypoScript', 'cozy_backend');
+}, 'sys_template', 'cozy_backend');
--- ext_localconf.php
+++ ext_localconf.php
@@ -2,27 +2,38 @@
 
 declare(strict_types=1);
 
-
+use Mblunck\CozyBackend\Component\ComponentCollection;
 use TYPO3\CMS\Core\Core\Environment;
 
 defined('TYPO3') || die();
 
-if (Environment::getContext()->isDevelopment()) {
-    $GLOBALS['TYPO3_CONF_VARS']['BE']['stylesheets']['cozy_backend'] =
-        'EXT:cozy_backend/Resources/Public/Backend/Css/Dev/';
-} else {
-    $GLOBALS['TYPO3_CONF_VARS']['BE']['stylesheets']['cozy_backend'] =
-        'EXT:cozy_backend/Resources/Public/Backend/Css/Live/';
-}
+call_user_func(
+    static function ($extensionKey): void {
+
+        if (Environment::getContext()->isDevelopment()) {
+            $GLOBALS['TYPO3_CONF_VARS']['BE']['stylesheets']['cozy_backend'] =
+                'EXT:cozy_backend/Resources/Public/Backend/Css/Dev/';
+        } else {
+            $GLOBALS['TYPO3_CONF_VARS']['BE']['stylesheets']['cozy_backend'] =
+                'EXT:cozy_backend/Resources/Public/Backend/Css/Live/';
+        }
 
 // Backend Login Customization
 //  system/additional.php
-$GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['backend'] = [
-    'backendFavicon' => 'EXT:cozy_backend/Resources/Public/Backend/Images/favicon.ico',
-    'backendLogo' => 'EXT:cozy_backend/Resources/Public/Backend/Images/icon.png',
-    'loginBackgroundImage' => 'EXT:cozy_backend/Resources/Public/Backend/Images/backend.jpg',
-    'loginFootnote' => date('Y') . '© by cozybackend',
-    'loginHighlightColor' => '#275b64',
-    'loginLogo' => 'EXT:cozy_backend/Resources/Public/Backend/Images/icon.png',
-    'loginLogoAlt' => 'Cozy Backend logo',
-];
+        $GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['backend'] = [
+            'backendFavicon' => 'EXT:cozy_backend/Resources/Public/Backend/Images/favicon.ico',
+            'backendLogo' => 'EXT:cozy_backend/Resources/Public/Backend/Images/icon.png',
+            'loginBackgroundImage' => 'EXT:cozy_backend/Resources/Public/Backend/Images/backend.jpg',
+            'loginFootnote' => date('Y') . '© by cozybackend',
+            'loginHighlightColor' => '#275b64',
+            'loginLogo' => 'EXT:cozy_backend/Resources/Public/Backend/Images/icon.png',
+            'loginLogoAlt' => 'Cozy Backend logo',
+        ];
+
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['fluid']['namespaces']['cozybackend'] = [
+            ComponentCollection::class
+        ];
+
+    },
+    'cozy_backend'
+);
