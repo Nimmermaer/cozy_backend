de16cf774addd07561086b42ad7a4d8ef1dc6836
Michael Blunck <michael.blunck@webicorns.com>
2025-12-07 10:20:14 +0100

    [TASK] adds site manifest

---
+++ Configuration/SiteConfiguration/Overrides/sites.php
@@ -0,0 +1,14 @@
+<?php
+
+$GLOBALS['SiteConfiguration']['site']['columns']['webmanifest_description'] = [
+    'label' => 'Manifest description',
+    'config' => [
+        'type' => 'text',
+    ],
+];
+
+$GLOBALS['SiteConfiguration']['site']['types']['0']['showitem'] = str_replace(
+    ', routes',
+    ', routes,--div--;Extended,webmanifest_description, ',
+    $GLOBALS['SiteConfiguration']['site']['types']['0']['showitem'],
+);
